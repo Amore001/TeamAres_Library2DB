3861b9062dec47e9b1f24c828f6f0ada65a9d9c0
PriS <priscillasouza.jor@gmail.com>
2023-03-28 17:22:30 -0600

    US03 Pri created

---
+++ src/test/java/com/library/steps/US03_PM_StepDefs.java
@@ -0,0 +1,57 @@
+package com.library.steps;
+
+import com.library.pages.BookPage;
+import com.library.pages.LoginPage;
+import com.library.utility.BrowserUtil;
+import com.library.utility.DB_Util;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+
+import java.util.List;
+
+public class US03_PM_StepDefs {
+
+    LoginPage loginPage = new LoginPage();
+    BookPage bookPage = new BookPage();
+    List<String> actualCategoryList;
+
+
+    @Given("the {string} on the home page")
+    public void the_on_the_home_page(String user) {
+        loginPage.login(user);
+        BrowserUtil.waitFor(2);
+    }
+    @When("the user navigates to {string} page")
+    public void the_user_navigates_to_page(String moduleName) {
+        new BookPage().navigateModule(moduleName);
+    }
+
+    @When("the user clicks book categories")
+    public void the_user_clicks_book_categories() {
+        bookPage.mainCategoryElement.click();
+    }
+
+    @Then("verify book categories must match book_categories table from db")
+    public void verify_book_categories_must_match_book_categories_table_from_db() {
+        //first step: create a query String to store our sql query
+        String query = "SELECT name from book_categories";
+        // 2 - run query to get data from db
+        DB_Util.runQuery(query);
+        // 3 - store this info inside the list >> EXPECTED
+        List<String> expectedCategoryList = DB_Util.getColumnDataAsList(1);
+
+        //4 - Get my ActualCategoryList
+        actualCategoryList = BrowserUtil.getAllSelectOptions(bookPage.mainCategoryElement);
+        //                                  method              WE element
+        actualCategoryList.remove(0);
+        //delete first option >>> ALL <<< so, the assert will match
+
+        //compare UI vs DB
+        Assert.assertEquals(expectedCategoryList, actualCategoryList);
+    }
+
+
+
+
+
+}
